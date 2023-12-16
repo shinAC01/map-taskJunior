527a41604fbd5712f892d4b9a828869a9fca4978
shinac01 <shinamv01@gmail.com>
2023-12-16 17:02:25 -0500

    Initializing repo

---
+++ src/main/java/com/juniortasks/demo/JuniorTask01Application.java
@@ -0,0 +1,13 @@
+package com.juniortasks.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorTask01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorTask01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/juniortasks/demo/JuniorTask01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniortasks.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorTask01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
