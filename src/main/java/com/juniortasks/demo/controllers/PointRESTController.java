1c6c9d093c10997c2763e7dd1ff37fb6694b8727
shinac01 <shinamv01@gmail.com>
2023-12-16 19:33:04 -0500

    adding a few test data and a getmapping method

--- src/main/java/com/juniortasks/demo/controllers/PointRESTController.java
+++ src/main/java/com/juniortasks/demo/controllers/PointRESTController.java
@@ -1,10 +1,14 @@
 package com.juniortasks.demo.controllers;
 
 
+import com.juniortasks.demo.entity.Point;
 import com.juniortasks.demo.services.IPointService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -12,4 +16,9 @@ public class PointRESTController {
 
     @Autowired
     private IPointService service;
+
+    @GetMapping("/recursos")
+    public List<Point> index() {
+        return service.findAll();
+    }
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO resources (date, lat, longitude) VALUES('2023-01-01', '45.17', '55.65');
+INSERT INTO resources (date, lat, longitude) VALUES('2023-01-01', '45.19', '55.67');
+INSERT INTO resources (date, lat, longitude) VALUES('2023-01-01', '45.21', '55.69');
+INSERT INTO resources (date, lat, longitude) VALUES('2023-01-01', '45.23', '55.71');
+INSERT INTO resources (date, lat, longitude) VALUES('2023-01-01', '45.25', '55.73');
